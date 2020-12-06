75694b074d29af35bd8ae2a81d4b863b91e50ce8
xiaotonghuang <xiaotong.huang@mail.utoronto.ca>
2020-12-05 21:04:07 -0500

    FORM IJ

--- app/src/main/java/com/example/a207_demo/entities/Conversation.java
+++ app/src/main/java/com/example/a207_demo/entities/Conversation.java
@@ -6,15 +6,16 @@ import java.util.HashSet;
 
 public class Conversation implements Serializable {
     private HashSet<String> userIds = new HashSet<String>(); //Store uerIds of the two users
-    private ArrayList<String[]> messages = new ArrayList<String[]>();
+    private ArrayList<String[]> messages = new ArrayList<String[]>(); //Store the messages of the conversation.
+    private ArrayList<String[]> unreadMessages = new ArrayList<>(); // Store the unread messages of the conversation
+    private ArrayList<String[]> archiveMessages = new ArrayList<>(); // Store the archive messages of the conversation.
 
     /**
      * The constructor of the conversation class
-     *
      * @param userId1 first user id
      * @param userId2 second user id.
      */
-    public Conversation(String userId1, String userId2) {
+    public Conversation(String userId1, String userId2){
         userIds.add(userId1);
         userIds.add(userId2);
     }
@@ -23,11 +24,36 @@ public class Conversation implements Serializable {
      * Add the sender's message to the list of messages.
      *
      * @param userId the user id of the sender of the message.
-     * @param text   the String of text the sender sends.
+     * @param text the String of text the sender sends.
      */
-    public void addMessage(String userId, String text) {
+    public void addMessage(String userId, String text){
         String[] message = {userId, text};
         messages.add(message);
+    }
+
+    /**
+     * Add a select message from messages list to the unread message list
+     * @param index the selected message
+     */
+    public void addUnreadMessage(int index){
+        String[] archiveMessage = messages.get(index);
+        unreadMessages.add(archiveMessage);
+    };
+
+    /**
+     * Getter of the unread message list
+     * @return the unread message list
+     */
+    public ArrayList<String[]> unreadMessageGetter(){
+        return unreadMessages;
+    }
+
+    /**
+     *
+     * Clear all the unread messages from the list
+     */
+    public void emptyUnreadMessage(){
+        unreadMessages.removeAll(unreadMessages);
     }
 
     /**
@@ -35,26 +61,57 @@ public class Conversation implements Serializable {
      *
      * @return the whole messages list of this conversation.
      */
-    public ArrayList<String[]> getMessages() {
+    public ArrayList<String[]> getMessages(){
         return messages;
     }
 
+    /**
+     * Delete a message of a given index
+     * @param index the index of the message in the messages list the user want to delete.
+     */
+    public void deleteMessage(int index){
+        messages.remove(index);
+    }
 
     /**
-     * 没写完
+     * Add a select message from messages list to the archive message list
+     * @param index the selected message
+     */
+    public void addArchiveMessages(int index){
+        String[] archiveMessage = messages.get(index);
+        archiveMessages.add(archiveMessage);
+    }
+
+    /**
+     * remove a select archive message
+     * @param index the selected message
+     */
+    public void deleteArchiveMessage(int index){
+        archiveMessages.remove(index);
+    }
+
+    /**
+     * Getter of the archive message list
+     * @return the archive message list
+     */
+    public ArrayList<String[]> archivedMessageGetter() {
+        return archiveMessages;
+    }
+
+    /**
      * Get the last messages of this conversation.
      *
      * @return the whole messages list of this conversation.
      */
-    public String[] getLastMessage() {
+    public String[] getLastMessage(){
         if (messages.size() > 0) {
             int lastIndex = messages.size() - 1;
-            Object[] userInfo = userIds.toArray();
-            String[] messageInfo = messages.get(lastIndex);
-            return new String[]{(String) userInfo[0], (String) userInfo[1], messageInfo[0], messageInfo[1]};
-        } else {
-            Object[] userInfo = userIds.toArray();
-            return new String[]{(String) userInfo[0], (String) userInfo[1], "", ""};
+            Object [] userInfo = userIds.toArray();
+            String [] messageInfo = messages.get(lastIndex);
+            return new String[]{(String)userInfo[0], (String)userInfo[1], messageInfo[0], messageInfo[1]};
+        }else{
+            Object [] userInfo = userIds.toArray();
+            return new String[]{(String)userInfo[0], (String)userInfo[1], "", ""};
         }
     }
 
@@ -63,7 +120,7 @@ public class Conversation implements Serializable {
      *
      * @return the cloned Hashset of userIds.
      */
-    public HashSet<String> getUserIds() {
+    public HashSet<String> getUserIds(){
 //        HashSet<String> cloneSet = (HashSet<String>)userIds.clone();
         return userIds;
     }

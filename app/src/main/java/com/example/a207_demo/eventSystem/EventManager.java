d412db3f73c237f66d3feabc616964b206e75feb
Jiayi Su <sjy.su@mail.utoronto.ca>
2020-12-06 10:35:12 -0500

    EventManager

--- app/src/main/java/com/example/a207_demo/eventSystem/EventManager.java
+++ app/src/main/java/com/example/a207_demo/eventSystem/EventManager.java
@@ -418,11 +418,6 @@ public class EventManager implements Serializable{
 
     //Todo: title is larger than 3 characters and not the same as other events
 
-    /**
-     * checkValidTitle
-     * @param title String title
-     * @return boolean
-     */
     public boolean checkValidTitle(String title){
         if (title.length() < 3) {
             return false;

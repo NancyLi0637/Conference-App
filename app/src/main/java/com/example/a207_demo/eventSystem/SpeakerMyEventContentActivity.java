9d7890fad533a9f920828d363c6480c91e471d4d
Deshang Kong <deshang.kong@mail.utoronto.ca>
2020-12-11 20:53:40 -0500

    Deleted import android.graphics.fonts.SystemFonts;

--- app/src/main/java/com/example/a207_demo/eventSystem/SpeakerMyEventContentActivity.java
+++ app/src/main/java/com/example/a207_demo/eventSystem/SpeakerMyEventContentActivity.java
@@ -1,7 +1,6 @@
 package com.example.a207_demo.eventSystem;
 
 import android.content.Intent;
-import android.graphics.fonts.SystemFonts;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
